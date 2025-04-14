753d1c49bb6e2f65efc7219eece9fbb841bd619b
AlbinCedenblad <albin.cedenblad@hotmail.com>
2025-04-14 14:12:07 +0200

    Db connection

--- DBCon/Program.cs
+++ DBCon/Program.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
+using System.Data.Common;
+using System.Reflection.Metadata;
 namespace Datalayer
 {
     public class Datalayer : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=sqlutb2-db.hb.se, 56077;Initial Catalog=***;Persist Security Info=True;User ID=***;Password=***;Trust Server Certificate=True");
+
+            optionsBuilder.UseSqlServer(@"Data Source=Sqlutb2-db.hb.se, 56077;Initial Catalog=dbtht2401;Persist Security Info=True;User ID=dbtht2401; Password=FMU279;Trust Server Certificate=True");
+            base.OnConfiguring(optionsBuilder);
         }
     }
+
+
 }
