cc2aaef6a237d0b98795ce730cebc2d1fbbbb6bd
niklasgullstrand <105774087+niklasgullstrand@users.noreply.github.com>
2025-04-17 14:20:22 +0200

    member entity

--- Model/Members.cs
+++ Model/Members.cs
@@ -4,6 +4,13 @@ namespace Model
 {
     internal class Members
     {
+		[Key]
+		public int member_ID { get; set; }
 
-    }
+		public string member_name { get; set; }
+		public date Birthday { get; set; }
+		public string phonenumber { get; set; }
+		public string email { get; set; }
+		public bool paymentstatus { get; set; }
+	}
 }
